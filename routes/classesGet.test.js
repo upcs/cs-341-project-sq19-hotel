92741a8c2dc3a7da27922611df1fc848f83db466
KaitlinLarson <larsonk20@up.edu>
2019-04-05 08:55:52 -0700

    callbacks in jest

---
+++ routes/classesGet.test.js
@@ -0,0 +1,11 @@
+// test that router.post sends the correct query, and the correct result
+
+const express = require('express');
+const router = express.Router();
+
+// send post request data,
+// expect to send "SELECT * FROM classes WHERE department = 'CS';"
+
+test('sends correct database query', () =>{
+    expect(dbms.dbquery).toHaveBeenCalledWith("SELECT * FROM classes WHERE department = 'CS'; ");
+});
